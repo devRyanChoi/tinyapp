53c5dbbb7e486c06becd0de426e8a4e34284659f
mingun choi <ryanchoi100826@gmail.com>
2023-01-22 23:29:07 -0500

    modified

--- express_server.js
+++ express_server.js
@@ -28,6 +28,6 @@ app.get("/urls", (req, res) => {
 });
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: req.params.longURL };
+  const templateVars = { id: req.params.id, longURL: "http://www.lighthouselabs.ca" };
   res.render("urls_show", templateVars);
 });
